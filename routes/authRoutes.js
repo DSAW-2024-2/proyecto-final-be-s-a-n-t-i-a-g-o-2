cf56fa7f7373460bfa17666048ce39fdb1ca2d0f
10santiago12 <surrego1303@gmail.com>
2024-10-24 16:52:05 -0500

    some-jwt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { register } = require('../controllers/register');
 const { login } = require('../controllers/login');
 const { logout } = require('../controllers/logout');
-const { erase } = require('../controllers/delete');
 
 const router = express.Router();
 
