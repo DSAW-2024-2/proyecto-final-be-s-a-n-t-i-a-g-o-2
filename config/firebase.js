ead6382efcad7449db061a57048b199704fe594a
10santiago12 <surrego1303@gmail.com>
2024-11-14 20:39:48 -0500

    firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,18 +1,18 @@
-const { initializeApp } = require('firebase/app');
-const { getAuth } = require('firebase/auth');
-const { getFirestore } = require('firebase/firestore');
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
 apiKey: "AIzaSyBn3OsHbdnZKZ2kIb6xRSvPVKSJ4aOAfZ4",
 authDomain: "proyecto-final-web-santiago-2.firebaseapp.com",
 projectId: "proyecto-final-web-santiago-2",
-storageBucket: "proyecto-final-web-santiago-2.appspot.com",
+storageBucket: "proyecto-final-web-santiago-2.firebasestorage.app",
 messagingSenderId: "769235505581",
 appId: "1:769235505581:web:ac1aeeecfc12ce8b55d124",
 measurementId: "G-5QFFW7HE3X"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
