6c356ebfb7cd8bf9e55b48ebb08aec61e6157b1f
10santiago12 <surrego1303@gmail.com>
2024-11-19 21:01:49 -0500

    get de cars

--- controllers/cars/get.js
+++ controllers/cars/get.js
@@ -1,21 +1,22 @@
-const { getFirestore, doc, getDoc } = require('firebase/firestore');
-const db = getFirestore();
+// controllers/cars/get.js
+const { db } = require('../../config/firebase');
+const { doc, getDoc } = require('firebase/firestore');
 
-const get =async (req, res) => {
+const get = async (req, res) => {
     const { vehicleuid } = req.params;
 
     try {
-        const driverDoc = await getDoc(doc(db, 'vehiculos', vehicleuid));
+    const vehicleDoc = await getDoc(doc(db, 'vehiculos', vehicleuid));
 
-        if (!driverDoc.exists()) {
-            return res.status(404).json({ error: 'Vehículo no encontrado.', code: 404 });
-        }
+    if (!vehicleDoc.exists()) {
+        return res.status(404).json({ error: 'Vehículo no encontrado.', code: 404 });
+    }
 
-        const vehicleData = driverDoc.data();
-        res.status(200).json({ vehicle: vehicleData });
-    }catch(error){
-        console.error('Error al obtener la información del vehículo:', error);
-        res.status(500).json({error: 'Error al obtener el vehículo', code: 500});
+    const vehicleData = vehicleDoc.data();
+    res.status(200).json({ vehicle: vehicleData });
+    } catch (error) {
+    console.error('Error al obtener la información del vehículo:', error);
+    res.status(500).json({ error: 'Error al obtener el vehículo', code: 500 });
     }
 };
 
